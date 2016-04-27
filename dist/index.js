3bf7f05f5768138007f7b5ee6c0b9c0aa09333b9
ean <eanplatter@gmail.com>
2016-04-26 19:42:18 -0500

    ...

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,12 @@
-"use strict";
+'use strict';
 
-var _shelljs = require("shelljs");
+var _shelljs = require('shelljs');
 
 var _shelljs2 = _interopRequireDefault(_shelljs);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log('dropped outa school now we done rich');
 
 _shelljs2.default.exec(process.argv[2]);
 var repeat = setInterval(function () {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import shell from 'shelljs'
 
+console.log('dropped outa school now we done rich')
+
 shell.exec(process.argv[2])
 const repeat = setInterval(() => {
   shell.exec("clear")
