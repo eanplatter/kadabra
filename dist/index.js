b1dfc825d91cce174b089ec43a178a8b0a04cb23
ean <eanplatter@gmail.com>
2016-04-26 19:50:35 -0500

    ...

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,11 @@ var _shelljs2 = _interopRequireDefault(_shelljs);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_shelljs2.default.exec(process.argv[2]);
+_shelljs2.default.exec("clear", { silent: true });
+
+{
+  silent: true;
+}_shelljs2.default.exec(process.argv[2]);
 var repeat = setInterval(function () {
   _shelljs2.default.exec("clear", { silent: true });
   _shelljs2.default.exec(process.argv[2], { silent: true });
