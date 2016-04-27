3287d3c79b68801520630adb4d7c9b87287f94c6
ean <eanplatter@gmail.com>
2016-04-26 19:39:07 -0500

    ...

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,12 @@
-"use strict";
+'use strict';
 
-var _shelljs = require("shelljs");
+var _shelljs = require('shelljs');
 
 var _shelljs2 = _interopRequireDefault(_shelljs);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+console.log('wat');
 _shelljs2.default.exec(process.argv[2]);
 var repeat = setInterval(function () {
   _shelljs2.default.exec("clear");
